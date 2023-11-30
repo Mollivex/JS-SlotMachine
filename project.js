c3dceee21fabc3c8fb2330949da949d2cdd24226
Alexandr Cocier <Limos12345@outlook.com>
2023-11-30 11:43:11 +0200

    Implementation of check and winning amount

--- project.js
+++ project.js
@@ -1,14 +1,31 @@
 // 1. Despot some money
-// 2. Determine number of lines to bet on
+// 2. Determine number of lines to bet on (1-3)
 // 3. Collect a bet amount
 // 4. Spin the slot machine
 // 5. Check if the user won
 // 6. Give the user their winnings
 // 7. Play again
 
+const prompt = require("prompt-sync")();
+
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_COUNT = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+const SYMBOLS_VALUES = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2
+ }
 
 // 1. Despot some money | starts here----->
-const prompt = require("prompt-sync")();
 const deposit = () =>{
     while(true){
         const depositAmount = prompt("Enter a deposit amount: ");
@@ -22,7 +39,6 @@ const deposit = () =>{
     }
 }
 // 1. Despot some money | ends here----->
-
 
 // 2. Determine number of lines to bet on | starts here--->
 const getNumberOfLines = () => {
@@ -54,6 +70,89 @@ const getBet = (balance, lines) =>{
 }
 // 3. Collect a bet amount | ends here----->
 
+// 4. Spin the slot machine | starts here----->
+const spin = () => {
+    const symbols = [];
+    for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)){
+       for (let i = 0; i < count; i++){
+           symbols.push(symbol);
+       }
+    }
+
+    const reels =[]
+    for (let i = 0; i < COLS; i++){
+        reels.push([]);
+       const reelSymbols = [...symbols];
+       for (let j =0; j < ROWS; j++){
+           const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+           const selectedSymbol = reelSymbols[randomIndex];
+           reels[i].push(selectedSymbol);
+           reelSymbols.splice(randomIndex, 1);
+       }
+    }
+    return reels;
+};
+
+const transpose = (reels) => {
+    const rows = [];
+
+    for(let i = 0; i < ROWS; i++){
+        rows.push([]);
+        for(let j = 0; j < COLS; j++){
+            rows[i].push(reels[j][i]);
+        }
+    }
+    return rows;
+}
+// 4. Spin the slot machine | ends here----->
+
+// 5. Check if the user won | starts here----->
+const printRows = (rows) => {
+    for (const row of rows){
+        let rowString = "";
+        for (const [i,symbol] of row.entries()){
+            rowString += symbol
+            if(i != row.length - 1){
+                rowString += " | "
+            }
+        }
+        console.log(rowString);
+    }
+}
+// 5. Check if the user won | end here----->
+
+// 6. Give the user their winnings | starts here----->
+const getWinnings = (rows, bet, lines) => {
+    let winnings = 0;
+
+    for (let row = 0; row < lines; row++){
+        const symbols = rows[row];
+        let allSame = true;
+
+        for(const symbol of symbols) {
+            if (symbol != symbols[0]) {
+                allSame = false;
+                break;
+            }
+        }
+
+        if (allSame){
+            winnings += bet * SYMBOLS_VALUES[symbols[0]];
+        }
+    }
+    return winnings;
+}
+// 6. Give the user their winnings | starts here----->
+
+
+// Game
 let balance = deposit();
 const numberofLines = getNumberOfLines();
 const bet = getBet(balance, numberofLines);
+const reels = spin();
+const rows = transpose(reels);
+printRows(rows);
+const winnings = getWinnings(rows, bet, numberofLines);
+console.log("You won, $" + winnings.toString());
+
+
